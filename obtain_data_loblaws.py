c4e915f6654c75bc06bcfac91e7f7bc2b92f7f29
Andrew <andrew.colinet@alum.utoronto.ca>
2024-05-19 23:33:20 -0400

    Updated scraper code to be faster.

--- obtain_data_loblaws.py
+++ obtain_data_loblaws.py
@@ -11,6 +11,9 @@ import time
 from collections import OrderedDict
 
 
+from requests_html import HTMLSession
+
+
 
 class Loblaws():
         
@@ -47,10 +50,10 @@ class Loblaws():
             # scroll to bottom of page
             driver.execute_script("window.scrollTo(0,1080*6);")
 
-            # # close privacy policy
-            # WebDriverWait(driver, 20).until(
-            #     EC.element_to_be_clickable((By.XPATH, "//button[@class='lds__privacy-policy__btnClose']"))
-            # ).click()
+            # close privacy policy
+            WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, "//button[@class='lds__privacy-policy__btnClose']"))
+            )#.click()
 
             # # click load more button
             # WebDriverWait(driver, 20).until(
@@ -92,8 +95,6 @@ class Loblaws():
             search_item_name = element.find_elements(By.XPATH, "//*[@class='product-name__item product-name__item--name']")
             search_item_price = element.find_elements(By.XPATH, "//*[@class='price selling-price-list__item__price selling-price-list__item__price--now-price'] | //*[@class='price selling-price-list__item__price selling-price-list__item__price--sale']")
 
-            # print([(brow.get_attribute("data-testid"), brow.text) for brow in search_item_eyebrow if brow.text != "New"])
-
             # get sponsor text but ignore 'new' and ignore repeat entries
             search_item_eyebrow = list(OrderedDict.fromkeys([(brow.get_attribute("data-testid"), re.sub(re.escape("New"), "", brow.text)) for brow in search_item_eyebrow]))
             search_item_eyebrow = [tup[1] for tup in search_item_eyebrow]
@@ -119,14 +120,74 @@ class Loblaws():
         df = pd.DataFrame(data=[list(row) for index, row in enumerate(zip(search_item_brand, search_item_name, search_item_price)) if search_item_eyebrow[index] != "Sponsored"], columns=columns)
 
         return df.sort_values(by=["brand", "price"], ignore_index=True)
-            
+    
+
+
+    def get_loblaws_data2(self, grocery_item: str):
+
+
+        # form url with query
+        url_query = self.url + "/search?search-bar=" + grocery_item
+
+        # get session object
+        session = HTMLSession()
+
+        # try to go to url
+        try:
+
+            r = session.get(url_query)
+
+            # render html and wait for html elements to appear
+            r.html.render(sleep=3)
+
+        except UnboundLocalError as e:
+            print(e)
+
+        finally:
+
+            # determine which items are sponspored
+            search_item_eyebrow = r.html.xpath("//*[@class='product-tile__eyebrow']")
+
+            # get brand, name, price, and unit of grocery item
+            search_item_brand = r.html.xpath("//*[@class='product-name__item product-name__item--brand']")
+            search_item_name = r.html.xpath("//*[@class='product-name__item product-name__item--name']")
+            search_item_price = r.html.xpath("//*[@class='price selling-price-list__item__price selling-price-list__item__price--now-price'] | //*[@class='price selling-price-list__item__price selling-price-list__item__price--sale']")
+
+            # # get sponsor text but ignore 'new' and ignore repeat entries
+            search_item_eyebrow = list(OrderedDict.fromkeys([(brow.attrs["data-testid"], re.sub(re.escape("New"), "", brow.text)) for brow in search_item_eyebrow]))
+            search_item_eyebrow = [tup[1] for tup in search_item_eyebrow]
+
+            # get brand text
+            search_item_brand = [brand.text for brand in search_item_brand]
+
+            # get item text
+            search_item_name = [name.text for name in search_item_name]
+
+            # format price
+            price_regex = r"(\.\d{2})(.*)"
+            search_item_price = [re.sub(price_regex, r'\1 \2', price.text) for price in search_item_price]
+
+
+            print(search_item_brand)
+            print(search_item_eyebrow)
+            print(search_item_name)
+            print(search_item_price)
+
+
+
 
 
 
 loblaws = Loblaws()
 
+# t0 = time.time()
+# print(loblaws.get_loblaws_deal_data("milk"))
+# print(time.time() - t0)
+
 t0 = time.time()
-print(loblaws.get_loblaws_deal_data("milk"))
+loblaws.get_loblaws_data2("milk")
 print(time.time() - t0)
+
+
 
 
